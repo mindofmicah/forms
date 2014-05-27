6d4249a985ee659451971349ad37602ebffcb802
Micah Eschbacher <michaeleschbacher@gmail.com>
2014-05-27 16:38:23 -0500

    These need to be public

--- src/MindOfMicah/Forms/FormServiceProvider.php
+++ src/MindOfMicah/Forms/FormServiceProvider.php
@@ -6,12 +6,12 @@ use Illuminate\Support\ServiceProvider;
 class FormServiceProvider extends ServiceProvider
 {
     protected $defer = false;
-    protected function boot()
+    public function boot()
     {
         $this->package('mindofmicah/forms');
     }
 
-    protected function register()
+    public function register()
     {
         $this->app['forms.validator'] = $this->app->share(function ($app) {
             $filesystem = $this->app->make('Illuminate\Filesystem\Filesystem');
