7e6cf5709a5f9f3269a60e74f63460c85ba32727
micah eschbacher <micah@whamdonk.com>
2014-05-27 17:17:13 -0400

    Initial Commit

---
+++ src/MindOfMicah/Forms/Commands/FormValidatorCommand.php
@@ -0,0 +1,100 @@
+<?php
+namespace MindOfMicah\Forms\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Illuminate\Filesystem\Filesystem;
+
+class FormValidatorCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'form:validator';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Create a new form validator.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(Filesystem $file)
+    {
+        $this->file = $file;
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+        $properties = explode(',',$this->ask('Which properties will you create rules for? Comma separated'));
+        $rules = [];
+        foreach ($properties as $property) {
+            $property = trim($property);
+            $rules[$property] = [];
+            $this->info('start listing the rules for "' . $property.'"');
+            while ($rule = $this->ask('')) {
+                $rules[$property][] = $rule;
+            }
+        }
+
+        $this->file->put(
+            app_path() . '/Whamdonk/Forms/' . $this->argument('name') . '.php',
+            str_replace(
+                ['{{$NAME$}}', '{{$RULES$}}'],
+                [$this->argument('name'), $this->formatRules($rules)],
+                $this->file->get(app_path() . '/commands/templates/stub.txt')
+            )
+        );
+		$this->info($this->argument('name') . ' was added as a validator');
+	}
+
+    private function formatRules(array $orig) {
+        $ret = '';
+        $glue = ",\n\t\t";
+        $lengths = array_map('strlen', array_keys($orig));
+        $num_spaces = max($lengths) + 1;
+        foreach($orig as $property => $rules) {
+
+            $ret.= $glue . "'" . $property . "'".str_repeat(' ', $num_spaces - strlen($property))."=> '" . implode('|', $rules) . "'";
+        }
+        return substr($ret, strlen($glue));
+    }
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			array('name', InputArgument::REQUIRED, 'Name of the new validator.'),
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+		);
+	}
+
+}
