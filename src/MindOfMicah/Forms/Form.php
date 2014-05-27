51cbebc72e4e56b78b4dd10e0a46cf0def4420bb
micah eschbacher <micah@whamdonk.com>
2014-05-27 15:17:43 -0400

    Initial Commit

---
+++ src/MindOfMicah/Forms/Form.php
@@ -0,0 +1,34 @@
+<?php
+namespace MindOfMicah\Forms;
+
+use Illuminate\Validation\Factory as Validator;
+
+
+abstract class Form
+{
+    private $validator;
+    
+    public function __construct(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function validate()
+    {
+        $this->validation = $this->validator->make($form_data, $this->getValidationRules());    
+        if ($this->validation->fails()) {
+            throw new FormValidationException('Validation Failed', $this->getValidationErrors();
+        }
+        return true;
+    }
+
+    protected function getValidationRules() 
+    {
+        return $this->rules;
+    }
+
+    protected function getValidationErrors()
+    {
+        return $this->validation->errors();
+    }
+}
---
+++ src/MindOfMicah/Forms/FormValidationException.php
@@ -0,0 +1,20 @@
+<?php
+namespace MindOfMicah\Forms;
+
+use Illuminate\Support\MessageBag;
+
+class FormValidationException extends \Exception
+{
+    protected $errors;
+
+    public function __construct($message, MessageBag $errors)
+    {
+        $this->errors = $errors;
+        parent::__construct($message);
+    }
+    
+    protected function getErrors()
+    {
+        return $this->errors;
+    }
+}
